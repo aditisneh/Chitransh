20117d3edc6a90cac035a6ca88cbd6e7f3af096c
Ankit Prakash <anku.verma895@gmail.com>
2021-11-01 02:54:48 +0530

    added memberlist data and table

---
+++ data/members.js
@@ -0,0 +1,342 @@
+let members =  [
+    {
+      "uid": 1,
+      "name": "Abhay Kumar Sinha",
+      "mob": "9431736135"
+    },
+    {
+      "uid": 2,
+      "name": "Abhay Kumar Srivastava",
+      "mob": "9279331457"
+    },
+    {
+      "uid": 3,
+      "name": "Abhishek Srivastava",
+      "mob": ""
+    },
+    {
+      "uid": 4,
+      "name": "Ajit Kumar Verma",
+      "mob": "9431355649"
+    },
+    {
+      "uid": 5,
+      "name": " Ankit Kumar Sinha",
+      "mob": "9934169817"
+    },
+    {
+      "uid": 6,
+      "name": "Anand Prakash Mehta",
+      "mob": "9431529877"
+    },
+    {
+      "uid": 7,
+      "name": "Anil Kumar Karan",
+      "mob": "9430757148"
+    },
+    {
+      "uid": 8,
+      "name": "Anil Kumar Prasad",
+      "mob": "8873280011"
+    },
+    {
+      "uid": 9,
+      "name": "Ashok Kumar Das",
+      "mob": "9031725778"
+    },
+    {
+      "uid": 10,
+      "name": "Ashok Prasad",
+      "mob": "266942"
+    },
+    {
+      "uid": 11,
+      "name": "Atul Srivastava",
+      "mob": ""
+    },
+    {
+      "uid": 12,
+      "name": "Bibhuti Bhushan",
+      "mob": "9835507962"
+    },
+    {
+      "uid": 13,
+      "name": "Chandan Kumar",
+      "mob": "9931305023"
+    },
+    {
+      "uid": 14,
+      "name": "D. N. Lal",
+      "mob": "9279859588"
+    },
+    {
+      "uid": 15,
+      "name": "Deonil Karna",
+      "mob": "9431509956"
+    },
+    {
+      "uid": 16,
+      "name": "Dhiraj Kumar",
+      "mob": ""
+    },
+    {
+      "uid": 17,
+      "name": "Dr.D. Kumar",
+      "mob": ""
+    },
+    {
+      "uid": 18,
+      "name": "J. N. Prasad",
+      "mob": ""
+    },
+    {
+      "uid": 19,
+      "name": "K. C. Prasad",
+      "mob": "7633886050"
+    },
+    {
+      "uid": 20,
+      "name": " K. P. Srivastava",
+      "mob": "9431566045"
+    },
+    {
+      "uid": 21,
+      "name": "Kameshwar Prasad",
+      "mob": "8986670431"
+    },
+    {
+      "uid": 22,
+      "name": "L. B. Prasad Verma",
+      "mob": "9199704961"
+    },
+    {
+      "uid": 23,
+      "name": "Mithlesh Kumar Sinha",
+      "mob": "9931350317"
+    },
+    {
+      "uid": 24,
+      "name": "Mukul Kumar Verma",
+      "mob": "8210190805"
+    },
+    {
+      "uid": 25,
+      "name": "Naresh Prasad Karan",
+      "mob": "7979720293"
+    },
+    {
+      "uid": 26,
+      "name": "Neel Ratna",
+      "mob": "8809650149"
+    },
+    {
+      "uid": 27,
+      "name": "Nishit Prasad",
+      "mob": "9955308308"
+    },
+    {
+      "uid": 28,
+      "name": "Niraj Sinha",
+      "mob": "9431079630"
+    },
+    {
+      "uid": 29,
+      "name": "O. P. Lal",
+      "mob": "9434764354"
+    },
+    {
+      "uid": 30,
+      "name": "P. K. Lala",
+      "mob": "9431566085"
+    },
+    {
+      "uid": 31,
+      "name": "P. K. Sinha",
+      "mob": "9308974537"
+    },
+    {
+      "uid": 32,
+      "name": "P. K. Sinha",
+      "mob": "9431324539"
+    },
+    {
+      "uid": 33,
+      "name": "P. P. Srivastava",
+      "mob": "9431310001"
+    },
+    {
+      "uid": 34,
+      "name": "Prabhat Kumar Sinha",
+      "mob": "266854"
+    },
+    {
+      "uid": 35,
+      "name": "Prabhat Kumar Lala",
+      "mob": "7484920499"
+    },
+    {
+      "uid": 36,
+      "name": "R. K. Sinha",
+      "mob": "9431794252"
+    },
+    {
+      "uid": 37,
+      "name": "R. P. Srivastava",
+      "mob": "9708193711"
+    },
+    {
+      "uid": 38,
+      "name": "R. R. Sinha",
+      "mob": "9430135733"
+    },
+    {
+      "uid": 39,
+      "name": "Rajesh Kumar Sinha",
+      "mob": "9931987096"
+    },
+    {
+      "uid": 40,
+      "name": "Rajiv Kumar Sinha",
+      "mob": "9934750284"
+    },
+    {
+      "uid": 41,
+      "name": "Rajiv Ranjan Sinha",
+      "mob": "9800016403"
+    },
+    {
+      "uid": 42,
+      "name": "Rupesh Srivastava",
+      "mob": "9931516877"
+    },
+    {
+      "uid": 43,
+      "name": "Mani Sinha",
+      "mob": "9431509938"
+    },
+    {
+      "uid": 44,
+      "name": "S. K. Srivastava",
+      "mob": ""
+    },
+    {
+      "uid": 45,
+      "name": "Shailendra Sinha",
+      "mob": "9771523261"
+    },
+    {
+      "uid": 46,
+      "name": "Sanjay Kumar Sinha",
+      "mob": "9470571366"
+    },
+    {
+      "uid": 47,
+      "name": "Sharad Nigam",
+      "mob": "9430309500"
+    },
+    {
+      "uid": 48,
+      "name": "Satish Chandra Srivastava",
+      "mob": "8987525704"
+    },
+    {
+      "uid": 49,
+      "name": "Satyajit Kumar Sinha",
+      "mob": "9431778007"
+    },
+    {
+      "uid": 50,
+      "name": "Satyanarayan Prasad",
+      "mob": "8651862312"
+    },
+    {
+      "uid": 51,
+      "name": "Saurav Punj",
+      "mob": ""
+    },
+    {
+      "uid": 52,
+      "name": "Santosh Srivastava",
+      "mob": "8102920519"
+    },
+    {
+      "uid": 53,
+      "name": "Shankar Lal",
+      "mob": "9955453848"
+    },
+    {
+      "uid": 54,
+      "name": "Subesh Choudhary",
+      "mob": "9031336726"
+    },
+    {
+      "uid": 55,
+      "name": "Sudhanshu Srivastava",
+      "mob": "7632022062"
+    },
+    {
+      "uid": 56,
+      "name": "Sumit Srivastava",
+      "mob": "8294116687"
+    },
+    {
+      "uid": 57,
+      "name": "Sunil Choudhary",
+      "mob": "9430785194"
+    },
+    {
+      "uid": 58,
+      "name": "Sunil Kumar Sinha",
+      "mob": "9934357214"
+    },
+    {
+      "uid": 59,
+      "name": "Sunil Kumar Sinha",
+      "mob": "9431721096"
+    },
+    {
+      "uid": 60,
+      "name": "Sunil Kumar (Chotu)",
+      "mob": "7903692882"
+    },
+    {
+      "uid": 61,
+      "name": "Sunil Kumar Karna",
+      "mob": "9430309495"
+    },
+    {
+      "uid": 62,
+      "name": "Umesh Kumar Srivastava",
+      "mob": ""
+    },
+    {
+      "uid": 63,
+      "name": "V. K. Srivastava",
+      "mob": "9431314875"
+    },
+    {
+      "uid": 64,
+      "name": "V. L. Srivastava",
+      "mob": "9835712237"
+    },
+    {
+      "uid": 65,
+      "name": "Vijay Kumar Verma",
+      "mob": "9031789250"
+    },
+    {
+      "uid": 66,
+      "name": "Vijay Prasad Sinha",
+      "mob": ""
+    },
+    {
+      "uid": 67,
+      "name": "Vinay Kumar Sinha",
+      "mob": "9934504824"
+    },
+    {
+      "uid": 68,
+      "name": "Vinod Kumar Srivastava",
+      "mob": "7295913661"
+    }
+  ]
